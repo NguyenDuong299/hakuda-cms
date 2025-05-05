353744d78604faf0b1d24038cdf36f599973bf7c
duongnt <ng.duong290901@gmail.com>
2025-05-05 23:35:53 +0700

    [feat] update cms

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
@@ -15,17 +11,10 @@ export default function AuthLayout({
           <div className="relative flex items-center justify-center z-1">
             {/* <!-- ===== Common Grid Shape Start ===== --> */}
             <div className="flex flex-col items-center max-w-xs">
-              <Link to="/" className="block mb-4">
-                <img
-                  width={231}
-                  height={48}
-                  src="/images/logo/auth-logo.svg"
-                  alt="Logo"
-                />
+              <Link to="/" className="block mb-4 text-white font-bold text-4xl">
+                Bandai
               </Link>
-              <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
+              <p className="text-center text-gray-400 dark:text-white/60">Admin Dashboard</p>
             </div>
           </div>
         </div>
